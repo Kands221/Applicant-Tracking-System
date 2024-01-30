bad6f1929efdacc783fe3fdc5b7a7ed1f8da1f33
kyle <andeskyle22@gmail.com>
2024-01-30 21:14:18 +0800

    #

--- job_posting/views.py
+++ job_posting/views.py
@@ -15,8 +15,6 @@ from .models import Job
 
 def job_detail(request, job_id):
     job = get_object_or_404(Job, id=job_id)
-    
-
     return render(request, 'job_posting/job_detail.html', {'job': job,})
 
 
